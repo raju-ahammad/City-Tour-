b327a58497c79e9c02a0438b9f5515bf3c1c575e
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-02 22:29:22 +0600

    Create City tour style

--- src/Components/Navbar/Navar.jsx
+++ src/Components/Navbar/Navar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import tour from "../../Data/img/tour.png"
 import "./Navbar.scss"
 
 const Navar = () => {
     return (
         <nav className="Navbar">
-            <h4 className="logo">City Tour</h4>
+            {/* <h4 className="logo">City Tour</h4> */}
+            <img src={tour} alt="" className="logo"/>
             <ul className="nav__links">
                 <li>
                     <a className="nav__link" href="#">Home</a>
